469e55b2d5a9aea96486051bcd1880a792fe39ee
Moez Bouhlel <bmoez.j@gmail.com>
2015-01-12 18:29:26 +0000

    update to lastest html5-video-everywhere code

--- data/youtube.js
+++ data/youtube.js
@@ -22,14 +22,11 @@
                 try {
                     if (player_container)
                         player_container.innerHTML = "";
-                    player_container = document.getElementById("player-mole-container");
-                    if (conf.isEmbed)
-                        player_container = document.body;
-                    if (conf.isChannel)
-                        player_container = document.getElementsByClassName("c4-player-container")[0];
+                    player_container = getPlayerContainer(conf);
                     if (!player_container)
                         return;
                     player_container.innerHTML = "";
+                    player_container.className = conf.className || "";
                     player = createNode("video", {
                         id: "video_player",
                         className: conf.className || "",
@@ -38,6 +35,8 @@
                         controls: true,
                         poster: conf.poster || "",
                         volume: OPTIONS.volume / 100
+                    }, {
+                        position: "relative"
                     });
                     player.appendChild(createNode("source", {
                         src: conf.url,
@@ -47,7 +46,63 @@
                 } catch (e) {
                     console.error("Exception on changePlayer()", e.lineNumber, e.columnNumber, e.message, e.stack);
                 }
+            })
+            .catch((rej) => {
+                if (rej === undefined)
+                    return;
+                switch (rej.error) {
+                    case "VIDEO_URL_UNACCESSIBLE":
+                        var error = rej.data.match(/reason=([^&]*)&/);
+                        if (error)
+                            errorMessage("Failed to load video url with the following error message: " +
+                                error[1].replace("+", " ", "g"));
+                        break;
+                    case "NO_SUPPORTED_VIDEO_FOUND":
+                        errorMessage("Failed to find any playable video url." +
+                            (rej.unsig ? " All urls are not signed" : ""), rej.conf);
+                        break;
+                }
             });
+    }
+
+    function errorMessage(msg, conf) {
+        logify("errorMessage", msg, conf);
+        var error_container;
+        if (conf)
+            error_container = getPlayerContainer(conf);
+        if (!error_container)
+            error_container = document.getElementById("player-unavailable") || document.getElementById("player");
+        if (!error_container)
+            return;
+        if (conf && conf.isWatch)
+            error_container.className += " player-height player-width";
+        if (conf && conf.isChannel)
+            error_container.className += " html5-main-video";
+        if (conf && conf.isEmbed) {
+            error_container.className += " full-frame";
+        }
+        error_container.style.background = "linear-gradient(to bottom, #383838 0px, #131313 100%) repeat scroll 0% 0% #262626";
+        error_container.innerHTML = "";
+        error_container.appendChild(createNode("p", {
+            textContent: "Ooops! :("
+        }, {
+            padding: "15px",
+            fontSize: "20px"
+        }));
+        error_container.appendChild(createNode("p", {
+            textContent: msg
+        }, {
+            fontSize: "20px"
+        }));
+    }
+
+    function getPlayerContainer(conf) {
+        if (conf.isWatch)
+            return document.getElementById("player-mole-container");
+        if (conf.isEmbed)
+            return document.body;
+        if (conf.isChannel)
+            return document.getElementsByClassName("c4-player-container")[0];
     }
 
     function getConfig() {
@@ -72,7 +127,9 @@
                 player_class = "player-width player-height";
             }
             if (!player_id)
-                reject();
+                reject({
+                    error: "PLAYER_ID_NOT_FOUND"
+                });
             resolve({
                 isEmbed: isEmbed,
                 isWatch: isWatch,
@@ -84,8 +141,18 @@
     }
 
     function getVideoInfo(conf) {
-        var INFO_URL = "https://www.youtube.com/get_video_info?hl=en_US&el=detailpage&video_id=";
+        var INFO_URL = "https://www.youtube.com/get_video_info?html5=1&hl=en_US&el=detailpage&video_id=";
         return asyncGet(INFO_URL + conf.id, {}, "text/plain").then((data) => {
+            if (data.endsWith("="))
+                try {
+                    data = atob(data);
+                } catch (_) {}
+            if (/status=fail/.test(data)) {
+                return Promise.reject({
+                    error: "VIDEO_URL_UNACCESSIBLE",
+                    data: data
+                });
+            }
             // get the poster url
             var poster = data.match(/iurlhq=([^&]*)/);
             if (poster)
@@ -93,7 +160,8 @@
             // extract avalable formats to fmts object
             var info = data.match(/url_encoded_fmt_stream_map=([^&]*)/)[1];
             info = decodeURIComponent(info);
-            var fmt, fmts = {};
+            var fmt, fmts = {},
+                unsignedVideos;
             info.split(",")
                 .map(it1 => {
                     var oo = {};
@@ -109,14 +177,19 @@
                 .filter(it6 => {
                     if (it6.url.search("signature=") > 0)
                         return true;
-                    logify("Url without signature!!", it6);
+                    unsignedVideos = true;
+                    logify("Url without signature!!", it6.itag);
                     return false;
                 })
                 .forEach(fmt => fmts[fmt.itag] = fmt);
             // choose best format from fmts onject
             fmt = getPreferredFmt(fmts, FMT_WRAPPER);
             if (fmt === undefined) {
-                return Promise.reject();
+                return Promise.reject({
+                    error: "NO_SUPPORTED_VIDEO_FOUND",
+                    unsig: unsignedVideos,
+                    conf: conf
+                });
             } else {
                 conf.url = fmt.url;
                 conf.type = fmt.type;
